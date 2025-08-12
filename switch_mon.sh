6ce6b7938226f7502622a013457a33b43e579396
Alan Hasty <gitlab@hasty.info>
2025-08-12 13:49:13 -0500

    fix:Remove check for registration

--- switch_mon.sh
+++ switch_mon.sh
@@ -8,15 +8,15 @@ script_to_run=/mnt/data/switch_detect.sh
 echo "Script to run is $script_to_run"
 echo "" 
 
-/mnt/data/led_red.sh ON
-/mnt/data/led_green.sh ON
+# /mnt/data/led_red.sh ON
+# /mnt/data/led_green.sh ON
 
-# Check the result of check_reg.py
-result=$(python3 /mnt/data/check_reg.py)
-if [ "$result" -eq 0 ]; then
-	python3 /mnt/data/led_blue.py ON
-	exit 0
-fi
+# # Check the result of check_reg.py
+# result=$(python3 /mnt/data/check_reg.py)
+# if [ "$result" -eq 0 ]; then
+# 	python3 /mnt/data/led_blue.py ON
+# 	exit 0
+# fi
 
 /mnt/data/led_red.sh OFF
 /mnt/data/led_green.sh ON
