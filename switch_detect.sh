2d646f6c5b3764f91ccc927045197bff7111e9f8
Alan Hasty <gitlab@hasty.info>
2025-08-12 11:31:20 -0500

    feat:Call connects, audio is now audible.
     On branch main
     Changes to be committed:
    	modified:   switch_detect.sh

--- switch_detect.sh
+++ switch_detect.sh
@@ -2,4 +2,17 @@
 
 echo "Switch press detected" 
 
+echo "Switch press detected"
+
+./led_green.sh OFF
+./led_red.sh ON
+
+
 aplay -D plughw:1,0 /mnt/data/sounds/ENU00209.wav
+#aplay -D plughw:1,0 /mnt/data/sounds/S0000303.wav
+sleep 1
+python3 test_serial.py -n 9723256826 -v True
+
+./led_red.sh OFF
+./led_green.sh ON
+
