eeea07cf5fccd9f59c8fab07d77aa62a12aef558
Alan Hasty <gitlab@hasty.info>
2025-09-04 17:47:11 -0500

    fix:Added comments and cleaned up test_serial call
    Added comments for identifying the different sounds. (speeds up testing)
    Change arguments to test_serial.py
    
     On branch br_check_reg
     Changes to be committed:
    	modified:   switch_detect.sh

--- switch_detect.sh
+++ switch_detect.sh
@@ -6,10 +6,15 @@ echo "Switch press detected"
 /mnt/data/led_red.sh ON
 
 
-#aplay -D plughw:1,0 /mnt/data/sounds/ENU00209.wav
-aplay -D plughw:1,0 /mnt/data/sounds/S0000303.wav
+# ENU00209.wav is the Emergency activation message
+aplay -D plughw:1,0 /mnt/data/sounds/ENU00209.wav
+
+# S0000303.wav is just the word "Delta" - useful for  testing.
+#aplay -D plughw:1,0 /mnt/data/sounds/S0000303.wav
 sleep 1
-python3 /mnt/data/test_serial.py -n 9723256826 -v True
+# Remove the -n <number> to use the default EDC number 
+# python3 /mnt/data/test_serial.py -v
+python3 /mnt/data/test_serial.py -n 9723256826 -v
 
 /mnt/data/led_red.sh OFF
 /mnt/data/led_green.sh ON
