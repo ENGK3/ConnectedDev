c57fa2dee31310f86db9af720a0107adffb5f476
Alan Hasty <gitlab@hasty.info>
2025-09-04 18:43:31 -0500

    feat:Got working with check_reg.py
    Now turns LED blue if the registration fails.
     On branch br_check_reg
     Changes to be committed:
    	modified:   switch_mon.sh

--- switch_mon.sh
+++ switch_mon.sh
@@ -3,16 +3,19 @@
 script_to_run=/mnt/data/switch_detect.sh
 
 echo "Script to run is $script_to_run"
-echo "" 
+echo ""
 
 if [ ! -e /tmp/setup ]; then
 	/mnt/data/led_red.sh ON
 	/mnt/data/led_green.sh ON
+	/mnt/data/led_blue.sh OFF
 
 	# Check the result of check_reg.py
-	result=$(python3 /mnt/data/check_reg.py)
-	if [ "$result" -eq 0 ]; then
-		python3 /mnt/data/led_blue.py ON
+	python3 /mnt/data/check_reg.py -q
+	if [ $? -ne 0 ]; then
+		/mnt/data/led_green OFF
+		/mnt/data/led_red.sh OFF
+		/mnt/data/led_blue.sh ON
 		exit 0
 	fi
 fi
@@ -20,7 +23,7 @@ fi
 /mnt/data/led_red.sh OFF
 /mnt/data/led_green.sh ON
 
-gpiomon --falling-edge --num-events=1 gpiochip1 24 | while read -r line; do 
+gpiomon --falling-edge --num-events=1 gpiochip1 24 | while read -r line; do
 	echo $0
 	$script_to_run
 done
