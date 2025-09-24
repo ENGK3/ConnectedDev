ac276e6484ba5314a153e933922b212f04db9852
Alan Hasty <gitlab@hasty.info>
2025-09-24 14:56:29 -0500

    fix:Changes for Gateworks

--- config_sys.sh
+++ config_sys.sh
@@ -1,6 +1,11 @@
 cp /mnt/data/99-ignore-modemmanager.rules  /etc/udev/rules.d/99-ignore-modemmanager.rules
 cp /mnt/data/switch_mon.service /etc/systemd/system/switch_mon.service
 
+cp /mnt/data/pulse/daemon.conf /etc/pulse/daemon.conf
+
+mkdir -p /home/kuser/.config/systemd/user
+cp /mnt/data/pulse/pulseaudio.service /home/kuser/.config/systemd/user/pulseaudio.service
+
 # systemctl stop weston.service
 # systemctl disable weston.service
 # systemctl stop connectcore-demo-server.service
@@ -8,6 +13,9 @@ cp /mnt/data/switch_mon.service /etc/systemd/system/switch_mon.service
 
 systemctl daemon-reload
 systemctl enable switch_mon.service
+# This must be done a kuser.
+#systemctl --user enable pulseaudio.service
+
 
 # Remount the /mnt/linux to be able to write to it.
 # umount /mnt/linux
