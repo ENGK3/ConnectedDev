557485c1fd6ebc826c71df2d0e293c773a551d7e
Alan Hasty <gitlab@hasty.info>
2025-10-15 12:46:18 -0500

    feat:Move files to the correct places

---
+++ VOIP/voip_config.sh
@@ -0,0 +1,9 @@
+cp /mnt/data/extensions.conf /etc/asterisk/extensions.conf
+cp /mnt/data/confbridge.conf /etc/asterisk/confbridge.conf
+cp /mnt/data/pjsip.conf /etc/asterisk/pjsip.conf
+chown asterisk:asterisk /etc/asterisk/extensions.conf /etc/asterisk/confbridge.conf \
+     /etc/asterisk/pjsip.conf
+
+cp /mnt/data/accounts /home/kuser/.baresip/accounts
+cp /mnt/data/basesip.config /home/kuser/.baresip/config
+chown kuser:kuser /home/kuser/.baresip/config /home/kuser/.baresip/accounts
