64d497cd23721a518ee7cd5bb41b33ad4b2a50eb
MCameron0 <mcameron@kingsiii.com>
2025-11-26 13:17:36 -0600

    Delete Pool Phone directory

--- Pool Phone/GW-Pool-V00.03.01/start.sh
+++
@@ -1,4 +0,0 @@
-docker run --rm -it --name dey-4.0-container \
-                   --security-opt seccomp=unconfined \
-                   --volume $HOST_ABSOLUTE_PATH_TO_WORKSPACE:/home/dey/workspace \
-                   digidotcom/dey:dey-4.0 bash
