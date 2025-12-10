ce133ad59c0ab9e6b6718ea9d91f710819f66116
Alan Hasty <gitlab@hasty.info>
2025-12-10 12:48:18 -0600

    feat:Added a check to see if device is reg.
     On branch br_ari_mon
     Changes to be committed:
    	modified:   modem_state.py

--- modem_state.py
+++ modem_state.py
@@ -1,10 +1,17 @@
 import logging
 import sys
+import time
 
 import serial
 
 # Import shared modem utilities
-from modem_utils import sbc_cmd_with_timeout, sbc_connect, sbc_disconnect
+from modem_utils import (
+    check_registration,
+    sbc_cmd_with_timeout,
+    sbc_connect,
+    sbc_disconnect,
+    start_reg_again,
+)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -37,6 +44,21 @@ serial_connection = serial.Serial()
 if not sbc_connect(serial_connection, port="/dev/ttyUSB3"):
     print("Failed to connect to modem")
     sys.exit(1)
+
+if check_registration(serial_connection, True):
+    logging.info("Modem is registered on the network")
+else:
+    logging.error("Modem is not registered on the network")
+
+    start_reg_again(serial_connection)
+
+    logging.info("Rechecking registration status... after 5 seconds")
+    time.sleep(5)
+    if check_registration(serial_connection, True):
+        logging.info("Modem is now registered on the network")
+    else:
+        sbc_disconnect(serial_connection)
+        sys.exit(1)
 
 
 for cmd in at_cmd_set:
