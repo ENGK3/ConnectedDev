ca7628fe4be78143d9854e080f300f88ea70835d
Alan Hasty <gitlab@hasty.info>
2026-01-12 14:54:04 -0600

    fix:Add missing package

--- kings3_install.sh
+++ kings3_install.sh
@@ -122,7 +122,7 @@ install_or_update_services() {
 if [ "$INSTALL_PACKAGES" = true ]; then
     echo "Installing packages..."
     apt-get install -y  python3-serial microcom pulseaudio btop \
-        python3-aiohttp python3-dotenv lm-sensors alsa-utils
+        python3-aiohttp python3-dotenv lm-sensors alsa-utils bc
 
     # Additional packages for elevator configuration.
     if [ "$CONFIG" == "elevator" ]; then
