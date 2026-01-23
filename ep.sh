6953ee191dc11272aec619adfa54fc42d8d48b60
Alan Hasty <gitlab@hasty.info>
2026-01-23 17:36:04 -0600

    fix:Removing top level ep.sh script

--- ep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ""
-asterisk -rx "pjsip show endpoints" \
-	| awk ' /Endpoint:/ {ep=$2;}
-	/Unavailable|Avail/ {state=$4;}
-	/Contact/ {contacts=$2; printf ">%s<>%s<>%s<\n", ep, state, contacts;}
-	' | grep -v CID
-echo ""
